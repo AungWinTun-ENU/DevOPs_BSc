9913e688ceb7adf29181ddd23e229ceaba5f3ed0
AungWinTun-ENU <40804628@live.napier.ac.uk>
2025-11-06 20:19:25 +0630

    Integration Testing

--- src/test/java/com/napier/devops/AppIntegrationTest.java
+++ src/test/java/com/napier/devops/AppIntegrationTest.java
@@ -8,22 +8,25 @@ import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class AppIntegrationTest
-{
+public class AppIntegrationTest {
     static App app;
 
     @BeforeAll
-    static void init()
-    {
+    static void init() {
         app = new App();
         app.connect("localhost:33060", 30000);
 
     }
 
     @Test
-    void testGetEmployee()
-    {
+    void testGetEmployee() {
         Employee emp = app.getEmployee(255530);
+
+        System.out.println("Employee found:");
+        System.out.println(" - ID: " + emp.emp_no);
+        System.out.println(" - First Name: " + emp.first_name);
+        System.out.println(" - Last Name: " + emp.last_name);
+
         assertEquals(emp.emp_no, 255530);
         assertEquals(emp.first_name, "Ronghao");
         assertEquals(emp.last_name, "Garigliano");
