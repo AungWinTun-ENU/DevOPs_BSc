f600cbc05fcbbd78c40de5c2fca3eb2cc1909be9
aungwintun-tzzf <aungwintun2005@gmail.com>
2025-11-17 15:49:40 +0630

    Lab 7 final part

--- src/test/java/com/napier/devops/AppIntegrationTest.java
+++ src/test/java/com/napier/devops/AppIntegrationTest.java
@@ -63,4 +63,18 @@ public class AppIntegrationTest
         assertTrue(allEmployees.size() > 0); // There should be employees in DB
     }
 
+    @Test
+    void testAddEmployee()
+    {
+        Employee emp = new Employee();
+        emp.emp_no = 500000;
+        emp.first_name = "Kevin";
+        emp.last_name = "Chalmers";
+        app.addEmployee(emp);
+        emp = app.getEmployee(500000);
+        assertEquals(emp.emp_no, 500000);
+        assertEquals(emp.first_name, "Kevin");
+        assertEquals(emp.last_name, "Chalmers");
+    }
+
 }
